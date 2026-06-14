fbdcbea6628efd5764d4626beea937a3d4fab00e
dayo777 <oruns@outlook.com>
2026-06-14 07:57:18 +0100

    commented out the outputs file containing removed variables declarations.

--- modules/networking/outputs.tf
+++ modules/networking/outputs.tf
@@ -1,19 +1,19 @@
-output "vpc_id" {
-  description = "ID of the VPC"
-  value       = module.vpc.vpc_id
-}
+# output "vpc_id" {
+#   description = "ID of the VPC"
+#   value       = module.vpc.vpc_id
+# }
 
-output "public_subnet_ids" {
-  description = "IDs of the public subnets"
-  value       = module.vpc.public_subnets
-}
+# output "public_subnet_ids" {
+#   description = "IDs of the public subnets"
+#   value       = module.vpc.public_subnets
+# }
 
-output "private_subnet_ids" {
-  description = "IDs of the private subnets"
-  value       = module.vpc.private_subnets
-}
+# output "private_subnet_ids" {
+#   description = "IDs of the private subnets"
+#   value       = module.vpc.private_subnets
+# }
 
-output "database_subnet_ids" {
-  description = "IDs of the database subnets"
-  value       = module.vpc.database_subnets
-}
+# output "database_subnet_ids" {
+#   description = "IDs of the database subnets"
+#   value       = module.vpc.database_subnets
+# }
